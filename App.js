7d634bd5dfb4d86523c6856cca1f43c7320535d5
Thales <thalesondas0@gmail.com>
2024-04-22 17:27:22 -0300

    Enfeitar Stack Navigator

--- App.js
+++ App.js
@@ -17,17 +17,17 @@ const App = () => {
 
     return(
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator screenOptions={{ headerStyle: { backgroundColor: '#21174c' }, headerTintColor: 'white' }}>
                 <Stack.Screen name='Login' component={Login} options={{ headerShown: false }}/>
-                <Stack.Screen name='NovaConta' component={NovaConta} />
-                <Stack.Screen name='RecuperarSenha' component={RecuperarSenha} />
+                <Stack.Screen name='NovaConta' component={NovaConta} options={{ headerTitle: 'Nova Conta' }}/>
+                <Stack.Screen name='RecuperarSenha' component={RecuperarSenha} options={{ headerTitle: 'Recuperar Senha' }}/>
                 <Stack.Screen name='HomePlaceholder' component={HomePlaceholder} options={{ headerShown: false }}/>
-                <Stack.Screen name='AcoesPesquisa' component={AcoesPesquisa} />
-                <Stack.Screen name='NovaPesquisa' component={NovaPesquisa} />
-                <Stack.Screen name='ModificarPesquisa' component={ModificarPesquisa} />
-                <Stack.Screen name='Coleta' component={Coleta} />
-                <Stack.Screen name='Relatorio' component={Relatorio} />
-                <Stack.Screen name='AgradecimentoParticipacao' component={AgradecimentoParticipacao} />
+                <Stack.Screen name='AcoesPesquisa' component={AcoesPesquisa} options={{ headerTitle: 'Ações da Pesquisa' }}/>
+                <Stack.Screen name='NovaPesquisa' component={NovaPesquisa} options={{ headerTitle: 'Criar uma Nova Pesquisa' }}/>
+                <Stack.Screen name='ModificarPesquisa' component={ModificarPesquisa} options={{ headerTitle: 'Modificar Pesquisa' }}/>
+                <Stack.Screen name='Coleta' component={Coleta} options={{ headerTitle: 'Coleta de Dados' }}/>
+                <Stack.Screen name='Relatorio' component={Relatorio} options={{ headerTitle: 'Relatório' }}/>
+                <Stack.Screen name='AgradecimentoParticipacao' component={AgradecimentoParticipacao} options={{ headerShown: false }}/>
             </Stack.Navigator>
         </NavigationContainer>
     )
