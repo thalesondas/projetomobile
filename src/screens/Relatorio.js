fdded1d0e4097190f290bd882fb218233abd78e6
Thales <thalesondas0@gmail.com>
2024-06-05 17:05:19 -0300

    Fazendo relatório pelo <PieChart> (ainda estático)

--- src/screens/Relatorio.js
+++ src/screens/Relatorio.js
@@ -1,14 +1,44 @@
-import { View, StyleSheet, Image } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { PieChart } from 'react-native-svg-charts'
 
 const Relatorio = () => {
-    return(
+    const data = [
+        {
+            key: 'Péssimo',
+            value: 50,
+            svg: { fill: '#53d8d8' },
+            arc: { outerRadius: '130%', cornerRadius: 10 }
+        },
+        {
+            key: 'Ruim',
+            value: 50,
+            svg: { fill: '#ea7288' }
+        },
+        {
+            key: 'Neutro',
+            value: 40,
+            svg: { fill: '#5fcda4' }
+        },
+        {
+            key: 'Bom',
+            value: 95,
+            svg: { fill: '#6994fe' }
+        },
+        {
+            key: 'Excelente',
+            value: 35,
+            svg: { fill: '#f1ce7e' }
+        }
+    ]
+
+    return (
         <View style={estilos.view}>
-
-            <Image
-                style={{ width: 400, height: 200 }}
-                source={require('../images/relatorioimg.png')}
+            <PieChart
+                style={{ height: 500 }}
+                outerRadius={'80%'}
+                innerRadius={15}
+                data={data}
             />
-
         </View>
     )
 }
